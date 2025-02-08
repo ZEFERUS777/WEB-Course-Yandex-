a2f4272f1916263e2ad1f9fd3b63cad8f0406aec
ZEFERUS777 <pomagomeddobri@gmail.com>
2025-02-08 20:15:48 +0300

    add file

--- WEB4/W1.py
+++ WEB4/W1.py
@@ -1,2 +1,16 @@
 import requests
 
+server = input().strip()
+port = int(input())
+a = int(input())
+b = int(input())
+
+url = f"{server}:{port}"
+params = {'a': a, 'b': b}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+sorted_result = sorted(data['result'])
+print(' '.join(map(str, sorted_result)))
+print(data['check'])

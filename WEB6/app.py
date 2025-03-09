f9b10e0fda623e97391a8752748612432039ee45
ZEFERUS777 <pomagomeddobri@gmail.com>
2025-03-09 07:53:48 +0300

    complete task api 2 UPD FILE app.py

--- WEB6/app.py
+++ WEB6/app.py
@@ -1,16 +1,16 @@
 import os
 import sys
+
 import requests
-
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QLineEdit
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap, QKeyEvent
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QLineEdit, QStatusBar
 from dotenv import load_dotenv
-
 
 from WEB4.cords_in import grad
 
 load_dotenv('keys.env')
-WIDTH, HEIGHT = (800, 600)
+WIDTH, HEIGHT = (650, 550)
 GEOCODER_API = os.getenv('GEOCODER_KEY')
 STATIC_API = os.getenv('STATIC_API')
 URL_STATIC = 'https://static-maps.yandex.ru/1.x/'
@@ -19,37 +19,49 @@ URL_GEOCODER = 'https://geocode-maps.yandex.ru/1.x'
 
 class Map_Window(QMainWindow):
     def __init__(self):
+        """inited window menu"""
         super().__init__()
-        self.setGeometry(0, 0, WIDTH, HEIGHT)
+        self.setGeometry(500, 300, WIDTH, HEIGHT)
         self.setWindowTitle('Yandex Maps')
 
+        """широта"""
         self.latitude = QLabel('latitude: ', self)
         self.latitude.move(WIDTH // 2 - 200, 20)
 
+        """широта ввод"""
         self.latitude_input = QLineEdit(self)
-        self.latitude_input.move(self.latitude.x() + 60, self.latitude.y())
-        self.latitude_input.resize(100, 23)
+        self.latitude_input.setGeometry(self.latitude.x() + 60, self.latitude.y(), 100, 23)  # задаю размер и позицию
 
+        """долгота"""
         self.longitude = QLabel('longitude: ', self)
         self.longitude.move(self.latitude_input.x() + self.latitude_input.width() + 20, self.latitude.y())
 
+        """долгота ввод"""
         self.longitude_input = QLineEdit(self)
-        self.longitude_input.move(self.longitude.x() + 70, self.longitude.y())
-        self.longitude_input.resize(100, 23)
+        self.longitude_input.setGeometry(self.longitude.x() + 70, self.longitude.y(), 100, 23)  # задаю размер и позицию
 
+        """кнопка получения карты и обработчик при нажатии"""
         self.get_map_btn = QPushButton('Enter', self)
         self.get_map_btn.move(self.longitude_input.x() + self.longitude_input.width() + 20,
                               self.longitude_input.y())
         self.get_map_btn.clicked.connect(self.get_map)
 
+        """карта"""
         self.map_img_lab = QLabel(self)
-        self.map_img_lab.move(0, self.longitude_input.y() + self.longitude_input.height() + 20)
-        self.map_img_lab.resize(WIDTH, HEIGHT - 100)  # Adjust size to fit window
+        self.map_img_lab.move(10, self.longitude_input.y() + self.longitude_input.height() + 20)
+        self.map_img_lab.resize(WIDTH, HEIGHT - 100)
+
+        """масштаб карты"""
+        self.scale = 0.1
+
+        self.status = QStatusBar(self, visible=True)
+        self.setStatusBar(self.status)
 
     def get_map(self):
-        lat = self.latitude_input.text()
-        lon = self.longitude_input.text()
+        lat = self.latitude_input.text()  # получение текста из поля широты
+        lon = self.longitude_input.text()  # получение текста из поля долготы
 
+        """обработчик ошибки при вводе неверных значений"""
         try:
             lat_float = float(lat)
             lon_float = float(lon)
@@ -59,7 +71,7 @@ class Map_Window(QMainWindow):
 
         spf = grad((lat_float, lon_float), GEOCODER_API)
 
-        req = f"{URL_STATIC}?apikey={STATIC_API}&ll={lon_float},{lat_float}&z=10&l=map&spn={spf[0]},{spf[1]}&z=21&theme=dark"
+        req = f"{URL_STATIC}?apikey={STATIC_API}&ll={lon_float},{lat_float}&z=10&l=map&spn={self.scale},{self.scale}&z=21&theme=dark"
         response = requests.get(req)
 
         if response.status_code == 200:
@@ -67,8 +79,17 @@ class Map_Window(QMainWindow):
             pixmap.loadFromData(response.content)
             self.map_img_lab.setPixmap(pixmap)
             self.map_img_lab.resize(pixmap.width(), pixmap.height())
+            print('successfully! ', self.scale)
+            self.status.showMessage(f'successfully!  {self.scale}')
         else:
             print(f"Failed to retrieve map. HTTP Status Code: {response.status_code}")
+
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key.Key_PageUp and self.scale <= 0.8:
+            self.scale *= 1.5
+        elif event.key() == Qt.Key.Key_PageDown and self.scale > 0.0011561019943888407:
+            self.scale /= 1.5
+        self.get_map()
 
 
 if __name__ == '__main__':
